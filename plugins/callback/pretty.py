8c46ba974977641ded2ef6a2749324d750f64cff
Nikita Chernyi <developer.nikus@gmail.com>
2018-06-05 17:09:26 +0300

    Added plugins, updated config, updated gitignore

---
+++ plugins/callback/pretty.py
@@ -0,0 +1,62 @@
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+DOCUMENTATION = '''
+    callback: debug
+    type: stdout
+    short_description: formated stdout/stderr display
+    description:
+      - Use this callback to sort though extensive debug output
+    version_added: "2.4"
+    extends_documentation_fragment:
+      - default_callback
+    requirements:
+      - set as stdout in configuration
+'''
+
+from ansible import constants as C
+from ansible.plugins.callback.unixy import CallbackModule as CallbackModule_unixy
+
+
+class CallbackModule(CallbackModule_unixy):  # pylint: disable=too-few-public-methods,no-init
+    CALLBACK_VERSION = 2.0
+    CALLBACK_TYPE = 'stdout'
+    CALLBACK_NAME = 'pretty'
+
+    def get_prefix(self, msg):
+        return {
+            'ok': '✔',
+            'done': '✔',
+            'unreachable': '✘',
+            'failed': '✘',
+        }.get(msg, '?')
+
+    def v2_playbook_on_task_start(self, task, is_conditional):
+        args = ''
+        if not task.no_log and C.DISPLAY_ARGS_TO_STDOUT:
+            args = u', '.join(u'%s=%s' % a for a in task.args.items())
+            args = u' %s' % args
+
+        self._display.display("➤ {}... {}".format(task.get_name().strip(), args))
+
+    def _process_result_output(self, result, msg):
+        host = result._host.get_name()
+        prefix = self.get_prefix(msg)
+        output = "{} {} {}".format(prefix, host, msg)
+        if self._run_is_verbose(result):
+            output = "{} {} {}: {}".format(prefix, host, msg, self._dump_results(result._result, indent=4))
+
+        if self.delegated_vars:
+            task_delegate_host = self.delegated_vars['ansible_host']
+            output = "{} {} -> {} {}".format(prefix, host, task_delegate_host, msg)
+
+        if result._result.get('msg') and result._result.get('msg') != "All items completed":
+            output = "{}: {}".format(output, result._result.get('msg'))
+
+        if result._result.get('stdout'):
+            output = "{} | stdout: {}".format(output, result._result.get('stdout'))
+
+        if result._result.get('stderr'):
+            output = "{} | stderr: {}".format(output, result._result.get('stderr'))
+
+        return output
